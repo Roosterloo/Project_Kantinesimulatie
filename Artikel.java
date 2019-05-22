b1aa309be881284be3f0b7c4b68a33e8f9129ce0
Ramon <ra.oosterloo@st.hanze.nl>
2019-05-22 12:33:47 +0200

    Week 1 Opdracht 1

---
+++ Artikel.java
@@ -0,0 +1,29 @@
+public class Artikel{
+
+    private String naam;
+    private int prijs;
+
+    public Artikel (String naam, int prijs){
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public Artikel(){
+    }
+
+    public String get_naam(){
+        return this.naam;
+    }
+
+    public int get_prijs(){
+        return this.prijs;
+    }
+
+    public void set_naam(String naam){
+        this.naam = naam;
+    }
+
+    public void set_prijs(int prijs){
+        this.prijs = prijs;
+    }
+}
