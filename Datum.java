4e67b12154ad3386aef3e00a947424d31f956806
Ronald <ronaldvanduinen99@gmail.com>
2019-05-22 11:56:06 +0200

    Er is aan datum gewerkt.

--- Datum.java
+++ Datum.java
@@ -5,23 +5,59 @@ public class Datum {
 	private int jaar;
 
 
-	public Datum() {
-		dag = 0;
-		maand = 0;
-		jaar = 0;
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
 	}
-	
+
+	public Datum() {
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
+	}
+
 	public boolean bestaatDatum(int dag, int maand, int jaar){
-		// TODO
+		if (dag >= 1 && maand >= 1 && maand <= 12 && jaar >= 1900 && jaar <= 2100){
+			switch (maand){
+
+			}
+				return true;
+
+		}
 		return false;
 	}
-	
+
 	/**
 	 * Getter voor Sting weergave van datum
 	 * @return Geboortedatum
 	 */
+
+	public int getDag() { return this.dag; }
+
+	public int getMaand() {
+		return this.maand;
+	}
+
+	public int getJaar() {
+		return this.jaar;
+	}
+
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
 	public String getDatumAsString() {
-		// TODO
-		return "";
+		int day = getDag();
+
+		return "day";
 		}
 }
