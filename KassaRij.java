ae0e3c228f95b71c818890c837f9f4f7f61ff291
Ramon <ra.oosterloo@st.hanze.nl>
2019-05-22 22:06:19 +0200

    Week 1 Opdracht 5 versie 0.9

--- KassaRij.java
+++ KassaRij.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class KassaRij {
+    private ArrayList<Dienblad> dienblad;
+    private Dienblad klant;
 
     /**
      * Constructor
      */
-    public KassaRij() {
-        // method body omitted
-    }
+    public KassaRij() { dienblad = new ArrayList<>();}
+
+    public KassaRij(Dienblad klant) { this.klant = klant;}
 
     /**
      * Persoon sluit achter in de rij aan
@@ -16,7 +18,7 @@ public class KassaRij {
      * @param klant
      */
     public void sluitAchteraan(Dienblad klant) {
-        // method body omitted
+        dienblad.add(klant);
     }
 
     /**
@@ -27,8 +29,13 @@ public class KassaRij {
      * @return Eerste klant in de rij of null
      */
     public Dienblad eerstePersoonInRij() {
-        // method body omitted
-    }
+        Dienblad volgende_klant = null;
+        if (erIsEenRij() == true){
+            volgende_klant = dienblad.get(0);
+            dienblad.remove(0);
+        }
+        return volgende_klant;
+        }
 
     /**
      * Methode kijkt of er personen in de rij staan.
@@ -36,6 +43,15 @@ public class KassaRij {
      * @return Of er wel of geen rij bestaat
      */
     public boolean erIsEenRij() {
-        // method body omitted
+        if (dienblad.size() == 0) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public void rijlengte_tostring(){
+        System.out.println("Er staan op het moment " + dienblad.size() + " mensen in de rij");
     }
 }
