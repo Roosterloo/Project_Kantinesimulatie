6640caf731493d7820eb2404f5d07df62f1e447c
Ronald <ronaldvanduinen99@gmail.com>
2019-05-22 15:45:56 +0200

    Klasse Datum Afgewerkt, Persoon alleen op D af.

--- Datum.java
+++ Datum.java
@@ -4,11 +4,13 @@ public class Datum {
 	private int maand;
 	private int jaar;
 
-
 	public Datum(int dag, int maand, int jaar) {
-		this.dag = dag;
-		this.maand = maand;
-		this.jaar = jaar;
+		this();
+		if (bestaatDatum(dag, maand, jaar)) {
+			this.dag = dag;
+			this.maand = maand;
+			this.jaar = jaar;
+		}
 	}
 
 	public Datum() {
@@ -17,13 +19,32 @@ public class Datum {
 		this.jaar = 0;
 	}
 
-	public boolean bestaatDatum(int dag, int maand, int jaar){
-		if (dag >= 1 && maand >= 1 && maand <= 12 && jaar >= 1900 && jaar <= 2100){
-			switch (maand){
-
-			}
+	public boolean bestaatDatum(int dag, int maand, int jaar) {
+		boolean isSchrikkelJaar = false;
+		if (dag > 0 && dag <= 31 && maand >= 1 && maand <= 12 && jaar >= 1900 && jaar <= 2100) {
+			if (maand == 1 || maand == 3 || maand == 5 || maand == 7 || maand == 8 || maand == 10 || maand == 12) {
 				return true;
+			} else if (maand == 4 || maand == 6 || maand == 9 || maand == 11 && dag < 31) {
+				return true;
+			}
+		} else if (maand == 2) {
+			int year = jaar % 4;
+			int year1 = jaar % 100;
+			int year2 = jaar % 400;
 
+			if (year == 0) {
+				isSchrikkelJaar = true;
+				if (year1 == 0) {
+					isSchrikkelJaar = false;
+					if (year2 == 0) {
+						isSchrikkelJaar = true;
+					}
+				}
+			}
+
+			if (isSchrikkelJaar && dag < 30 || !isSchrikkelJaar && dag < 29) {
+				return true;
+			}
 		}
 		return false;
 	}
@@ -56,8 +77,6 @@ public class Datum {
 	}
 
 	public String getDatumAsString() {
-		int day = getDag();
-
-		return "day";
+		return getDag() + ":" + getMaand() + ":" + getJaar();
 		}
 }
--- Persoon.java
+++ Persoon.java
@@ -3,14 +3,74 @@ public class Persoon{
     private int BSN;
     private String voornaam;
     private String achternaam;
-    private int geboortedatum;
+    private Datum geboortedatum;;
     private char geslacht;
 
-    public Persoon(int BSN, String voornaam, String achternaam, int geboortedatum, char geslacht) {
+    public Persoon(int BSN, String voornaam, String achternaam, Datum geboortedatum, char geslacht) {
         this.BSN = BSN;
         this.voornaam = voornaam;
         this.achternaam = achternaam;
         this.geboortedatum = geboortedatum;
-        this.geslacht = geslacht;
+        setGeslacht(geslacht);
+    }
+
+    public Persoon() {
+        this.BSN = 0;
+        this.voornaam = "";
+        this.achternaam = "";
+        this.geboortedatum = null;
+        this.geslacht = 'a';
+
+    }
+
+    public void setBSN(int BSN) {
+        this.BSN = BSN;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public void setGeboortedatum(Datum geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public void setGeslacht(char geslacht) {
+        geslacht = Character.toUpperCase(geslacht);
+        switch (geslacht){
+            case 'M': this.geslacht = geslacht; break;
+            case 'V': this.geslacht = geslacht; break;
+            default: System.out.println("De waarde is niet goed gezet!"); break;
+        }
+    }
+
+    public int getBSN() {
+        return BSN;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public String getGeboortedatum() {
+        return geboortedatum.getDatumAsString();
+    }
+
+    public String getGeslacht() {
+        if(geslacht == 'M'){
+            return "Man";
+        } else if (geslacht == 'V'){
+            return "Vrouw";
+        } else{
+            return "Onbekend";
+        }
     }
 }
