581a39032efa411b3ccf95cf90cada8b1e261c45
Ronald <ronaldvanduinen99@gmail.com>
2019-06-13 16:22:50 +0200

    Administratie (Opgave 2) afgemaakt.

--- Administratie.java
+++ Administratie.java
@@ -1,15 +1,17 @@
 public class Administratie {
 
+
+    private static final int DAYS_IN_WEEK = 7;
+
+    private Administratie(){
+    }
+
     /**
      * Deze methode berekent van de int array aantal de gemiddelde waarde
      *
      * @param //aantal
      * @return het gemiddelde
      */
-
-    private Administratie(){
-    }
-
     public static double berekenGemiddeldAantal(int[] aantal) {
         int totaal = 0;
         for(int waarde : aantal){
@@ -21,7 +23,7 @@ public class Administratie {
     /**
      * Deze methode berekent van de double array omzet de gemiddelde waarde
      *
-     * @param omzet
+     * @param //omzet
      * @return het gemiddelde
      */
     public static double berekenGemiddeldeOmzet(double[] omzet) {
@@ -35,20 +37,17 @@ public class Administratie {
     /**
      * Methode om dagomzet uit te rekenen
      *
-     * @param omzet
+     * @param //omzet
      * @return array (7 elementen) met dagomzetten
      */
 
     public static double[] berekenDagOmzet(double[] omzet) {
-        double[] temp = new double[7];
-        for(int i = 0; i < 7; i++) {
-
+        double[] temp = new double[DAYS_IN_WEEK];
+        for(int i = 0; i < DAYS_IN_WEEK; i++) {
             int j = 0;
-            while(j > 0) {
-                temp[i] += omzet[i + 7 * j];
-
-                // omitted
-
+            while((i + DAYS_IN_WEEK * j) <= omzet.length - 1) {
+                temp[i] += omzet[i + DAYS_IN_WEEK * j];
+                j++;
             }
         }
         return temp;
